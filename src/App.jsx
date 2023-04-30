f8a85f2de95e3d95fb1c760d1c1d867c67eda5b8
Iryna Fedorova <i.fedorova4321@gmailcom>
2023-04-30 22:38:58 +0200

    Resume

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ export default () => {
   return <div>
     <nav className='navbar' ref={sideNavRef}> 
       <div className={menu}>
-        <NavLink to='/home'>Home</NavLink>
+        <NavLink to='/'>Home</NavLink>
         <NavLink to='/portfolio'>Portfolio</NavLink>
         <NavLink to='/resume'>Resume</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
